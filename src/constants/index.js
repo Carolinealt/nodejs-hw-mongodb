7a64896e5fe74a65de02c0e7de0ba2df405df1b9
Caroline Kudrytska <e.scaroline02@gmail.com>
2024-11-16 21:42:03 +0200

    cloudinary

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@ export const SORT_ORDER = {
     DESC: 'desc'
 }
 
-export const ACCESS_TOKEN_TTL = 30*15 * 60 * 1000 // 30*15 min in ms
+export const ACCESS_TOKEN_TTL = 30 * 15 * 60 * 1000 // 30*15 min in ms
 export const REFRESH_TOKEN_TTL = 30 * 24 * 60 * 60 * 1000 // 30 day in ms
 
 export const SMTP = {
@@ -21,3 +21,9 @@ export const TEMPLATES_DIR = path.resolve("src", "templates");
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
+export const CLOUDINARY = {
+    CLOUD_NAME: process.env.CLOUD_NAME,
+    API_KEY: process.env.API_KEY,
+    API_SECRET: process.env.API_SECRET,
+    ENABLE_CLOUDINARY: process.env.ENABLE_CLOUDINARY,
+};
